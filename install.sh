7bfaf8b09ef1ed54739e54c5c30d913ab0d388da
pinu <115863127+pinugit@users.noreply.github.com>
2024-07-17 16:17:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,12 +23,21 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 # Add Neovim to PATH
-export PATH="$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
-source ~/.bashrc
+
 
 # Clone Neovim configuration
 git clone https://github.com/pinugit/my-neovim-config.git ~/.config/nvim
 rm lazygit.tar.gz ripgrep_14.1.0-1_amd64.deb nvim-linux64.tar.gz install.sh
 
+
 # Launch Neovim
+echo "
+
+
+"
+echo '
+export PATH="$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
+source ~/.bashrc
 nvim
+'
+
